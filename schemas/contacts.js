f721e7606258c82a7d9e2c4aa6a50a75edb87427
Anna <a.zaliubovska@gmail.com>
2023-05-02 21:49:47 +0300

    addedchange

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,22 +1,5 @@
 const Joi = require('joi');
 
-// const schemePost = Joi.object({
-//     name: Joi.string().required(),
-//     email: Joi.string().required(),
-//     phone: Joi.string().required(),
-// });
-
-// const schemePut = Joi.object({
-//     name: Joi.string().required(),
-//     email: Joi.string().required(),
-//     phone: Joi.string().required(),
-// });
-
-
-// module.exports = {
-//     schemePost, schemePut
-// };
-
 
 const addSchema = Joi.object({
     name: Joi.string().required(),
