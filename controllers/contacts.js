27d636757b4944a1ee424d413d489ac3a579dfc5
Anna <a.zaliubovska@gmail.com>
2023-05-07 14:58:31 +0300

    add

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+// const Joi = require('joi');
 
 const contacts = require('../models/contacts');
 
@@ -7,7 +7,7 @@ const { HttpError, ctrWrapper } = require('../helpers');
 const getAll = async (req, res) => {
       const result = await contacts.listContacts();
       res.json(result);
-  }
+  };
 
 const getById = async (req, res) => {
       const { id } = req.params;
@@ -16,41 +16,37 @@ const getById = async (req, res) => {
           throw HttpError(404, 'Not found');
       }
       res.json(result);
-  }
+  };
 const addContact = async (req, res) => {
     const result = await contacts.addContact(req.body);
     res.status(201).json(result);
-    if (!result) {
-      res.status(400).json({"message": "missing required name field"})
-    }
+    // if (!result) {
+    //   res.status(400).json({"message": "missing required name field"})
+    // }
 
-}
+};
 const deleteById = async (req, res) => {
   const { id } = req.params;
   const  result = await contacts.removeContact(id);
   if (!result) {
-      throw HttpError(404, 'Not found');
+      return res.status(404).json({ message: "Not found" });
   }
-  res.status(200).json({message: 'Delet success'});
-}
+  res.status(200).json({message: 'contact deleted'});
+};
 const updateById = async (req, res) => {
-    const { name, email, phone } = req.body;
     const { id } = req.params;
-    if (!name && !email && !phone) {
-        res.status(400).json({"message": "missing fields"})
-    }
     const result = await contacts.updateContact(id, req.body);
     if (!result) {
         return res.status(404).json ({
           "message": "Not found"
         });
     }
-    res.status(200).json(result);
+    res.status(200).json( {result} );
 }
   module.exports = {
     getAll: ctrWrapper(getAll),
     getById: ctrWrapper(getById),
     addContact: ctrWrapper(addContact),
-    updateById: ctrWrapper(updateById),
     deleteById: ctrWrapper(deleteById),
+    updateById: ctrWrapper(updateById),
   }
