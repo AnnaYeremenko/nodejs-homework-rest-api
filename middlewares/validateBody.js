ef2a3b2d3c489f874e16b2f7ba16f52bfa09407b
Anna <a.zaliubovska@gmail.com>
2023-05-07 11:29:39 +0300

    added changes

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,16 +1,36 @@
-const contacts = require('../models/contacts');
+const Joi = require('joi');
 
 const {HttpError} = require('../helpers');
 
-const validateBody = schema => {
-    const func = (req, res, next)=> {
-        const { error } = schema.validate(req.body);
-        if (error) {
-            next(HttpError(400, error.message));
+module.exports = {
+putContactValidation: (req, res, next ) => {
+    const contactSchema = Joi.object({
+        name: Joi.string().min(3).max(30).required(),
+      email: Joi.string().email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+      }),
+      phone: Joi.string().min(14).max(14).required(),
+    });
+const validationResult = contactSchema.validate(req.body);
+        if (validationResult.error) {
+            return (HttpError(400, error.message));
         }
-        next()
-    }
-    return func;
-}
-
-module.exports = validateBody;
+        next();
+    },
+postContactValidation: (req, res, next ) => {
+        const contactSchema = Joi.object({
+            name: Joi.string().min(3).max(30).required(),
+      email: Joi.string().email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+      }),
+      phone: Joi.string().min(14).max(14).required(),
+    });
+const validationResult = contactSchema.validate(req.body);
+        if (validationResult.error) {
+            return (HttpError(400, error.message));
+            }
+            next();
+        },
+    };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,24 @@
 const express = require('express');
+const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
+const {
+    putContactValidation,
+    postContactValidation
+} = require ("../../middlewares/validateBody");
 
-const {validateBody} = require("../../middlewares");
+// const {validateBody} = require("../../middlewares");
 
-const schemas = require("../../schemas/contacts");
-
-const router = express.Router();
+// const schemas = require("../../schemas/contacts");
 
 router.get('/', ctrl.getAll);
 
 router.get('/:id', ctrl.getById);
 
-router.post('/', validateBody(schemas.addSchema), ctrl.addContact);
+router.post('/', postContactValidation, ctrl.addContact);
 
 router.delete('/:id', ctrl.deleteById);
 
-router.put('/:id', validateBody(schemas.addSchema), ctrl.updateById);
+router.put('/:id', putContactValidation, ctrl.updateById);
 
 module.exports = router;
