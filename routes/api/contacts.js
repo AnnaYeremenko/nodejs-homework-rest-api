6cf28dcdc427d9bf2b57b5c26b6b440b6494338b
Anna <a.zaliubovska@gmail.com>
2023-07-05 13:39:25 +0300

    add

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,23 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const ctrl = require("../../controllers/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {validateBody, isValidId, authenticate} = require("../../middlewares");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {schemas} = require("../../schemas/contacts");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', authenticate, ctrl.getAll);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:id', authenticate, isValidId, ctrl.getContactById);
 
-module.exports = router
+router.post('/', authenticate, validateBody(schemas.schemePost), ctrl.add);
+
+router.put('/:id', authenticate, isValidId, validateBody(schemas.schemePut), ctrl.updateById);
+
+router.patch('/:id/favorite', authenticate, isValidId, validateBody(schemas.schemePatch), ctrl.updateFavorite);
+
+router.delete('/:id', authenticate, isValidId, ctrl.deleteById);
+
+module.exports = router;
